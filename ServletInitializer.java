c4169511a51459786797daed2474e2447be885f8
Varun-SecurDI <124686864+Varun-SecurDI@users.noreply.github.com>
2023-04-24 12:52:01 +0530

    Add files via upload

---
+++ ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.spring.azure;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AzureApplication.class);
+	}
+
+}
